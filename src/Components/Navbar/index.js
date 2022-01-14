d6d351d6121694ea191da27637e86dcaa8b987ac
Franco <francomelgar4@gmail.com>
2022-01-14 15:47:27 -0500

    about fixed

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -11,7 +11,7 @@ const Navbar = () => (
         <a href="/">Home</a>
       </li>
       <li>
-        <a href="about">About</a>
+        <a href="/about">About</a>
       </li>
     </ul>
   </header>
